69efc87816e3b772f246587355d278fb887990a5
Phong Phan <fongfan999@gmail.com>
2017-05-12 08:23:47 +0700

    Refactored 'get product urls' method

--- lib/lazada/scraper.rb
+++ lib/lazada/scraper.rb
@@ -2,21 +2,25 @@ require 'nokogiri'
 require 'open-uri'
 require 'json'
 
-class LzdScraper 
+class LzdScraper
+  HOST = 'http://www.lazada.vn'
+  CAMERA_URLS = "#{HOST}/may-anh-may-quay-phim/?itemperpage=120"
+
   def initialize(page)
     @page = page.to_s
-    @doc = Nokogiri::HTML(open("http://www.lazada.vn/may-anh-may-quay-phim/?itemperpage=120&page=#{@page}&searchContext=category"))
-    @product_paths = get_product_paths
   end
 
   def get_all_reviews
+    product_urls = get_product_urls
     reviews = []
     count = 1
-    @product_paths.each do |path|
+
+    product_urls.each do |url|
       puts count
       count += 1
+
       review = {}
-      review[:product_name] = path.split('-').join(' ')
+      review[:product_name] = url.split('-').join(' ')
       review[:content] = get_review_per_page(path)
       reviews << review
     end
@@ -31,7 +35,9 @@ class LzdScraper
       doc.css('.ratRev_reviewListRow>.ratRev_revDetail').text
     end
 
-    def get_product_paths
-      @doc.css('.c-product-list>.c-product-card>.c-product-card__img-placeholder>a').map { |e| e.values.first  }
-    end  
+    def get_product_urls
+      doc = Nokogiri::HTML open(CAMERA_URLS)
+      doc.css('.c-product-card__name')
+        .map { |name| HOST + name.attr('href').strip }
+    end
 end
