fc897005cd586ea48e8f9477335a6312be5cf3ea
Phong Phan <fongfan999@gmail.com>
2017-05-12 13:55:21 +0700

    Add method 'get_data_from_sku'

--- lib/lazada/scraper.rb
+++ lib/lazada/scraper.rb
@@ -5,6 +5,25 @@ require 'json'
 class LzdScraper
   HOST = 'http://www.lazada.vn'
   CAMERA_URLS = "#{HOST}/may-anh-may-quay-phim/?itemperpage=120"
+  REVIEW_URLS = "#{HOST}/ajax/ratingreview/reviewspage"
+
+  def self.get_data_from(sku)
+    data = []
+    page = 1
+
+    loop do
+      reviews = JSON.parse open("#{REVIEW_URLS}?page=#{page}&sku=#{sku}", &:read)
+      data << reviews['data']['ratingReviews']
+        .select { |r| r['avg_rating'] > 2 && !r['detail'].empty? }
+        .map { |r| r['detail'] }
+
+      break unless reviews['data']['paginator']['next']
+
+      page += 1
+    end
+
+    return data.first
+  end
 
   def initialize(page)
     @page = page.to_s
@@ -25,6 +44,16 @@ class LzdScraper
       reviews << review
     end
     reviews
+  end
+
+  def scrape_reviews
+    @data = []
+    skus.each do |name, sku|
+      @data << {
+        name: name,
+        sku: LzdScraper.get_data_from(sku)
+      }
+    end
   end
 
   private
--- main.rb
+++ main.rb
@@ -1,6 +1,10 @@
 require_relative 'lib/lazada/scraper'
 
-page = 1
-puts "page #{page}"
-ex = LzdScraper.new(page)
-File.write("lib/data/#{page}.json", ex.get_all_reviews.to_json)
+# page = 1
+# puts "page #{page}"
+# ex = LzdScraper.new(page)
+# File.write("lib/data/#{page}.json", ex.get_all_reviews.to_json)
+#
+
+sku = 'NE644ELAA1U825VNAMZ'
+p LzdScraper.get_data_from(sku)
